01be5d4e2e6bcc5980712179f509fd979102bcc7
Amir Farhadi <amirf147@gmail.com>
2024-05-31 16:00:52 +0300

    create firefox extended rule, add navigation transforms
    created a firefox rule that would just include things that are not included in the builtin one, in this case the the ability to press control and a number for navigation.
    added my preferred phrases for copying cutting and pasting and added transform for pressing shift tab

---
+++ caster_user_content/rules/firefox_extended_rule.py
@@ -0,0 +1,25 @@
+from dragonfly import Repeat, Pause, Function, Choice, MappingRule, IntegerRef
+
+from castervoice.lib.actions import Key, Mouse, Text
+
+from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
+from castervoice.lib.merge.state.short import R
+
+from castervoice.lib import github_automation
+from castervoice.lib.temporary import Store, Retrieve
+
+class FirefoxExtendedRule(MappingRule):
+    pronunciation = "extended fire fox"
+    mapping = {
+        "page <n>":
+            R(Key("c-%(n)d")),
+    }
+    extras = [
+        IntegerRef("n", 1, 9),
+    ]
+    defaults = {
+        "n": 1
+    }
+
+def get_rule():
+    return FirefoxExtendedRule, RuleDetails(name="fire fox extended", executable="firefox")
