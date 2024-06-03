eefef0468ff719974ff0a7d512e473ed0a65c27a
Amir Farhadi <amirf147@gmail.com>
2024-06-03 17:06:17 +0300

    Update global_ccr_extended_rule.py
    added a phrase mapping for go back, i.e. a-left

--- caster_user_content/rules/global_ccr_extended_rule.py
+++ caster_user_content/rules/global_ccr_extended_rule.py
@@ -7,9 +7,18 @@ from castervoice.lib.merge.mergerule import MergeRule
 from castervoice.lib.const import CCRType
 
 class GlobalCCRExtendedRule(MergeRule):
+
     pronunciation = "global ccr extended"
+
     mapping = {
         "flash": R(Key("f2")),
+        "go back [<n>]": R(Key("a-left:%(n)d"))
+    }
+    extras = [
+        ShortIntegerRef("n", 1, 10),
+    ]
+    defaults = {
+        "n": 1 
     }
 
 def get_rule():
