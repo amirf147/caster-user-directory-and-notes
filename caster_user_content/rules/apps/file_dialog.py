137ed82c86062be7a3111fc7bef586bd9b7ee1f2
Amir Farhadi <amirf147@gmail.com>
2024-06-06 01:21:40 +0300

    create custom file dialog rule and add new bring me app
    i just copied over the file dialog rule with my own customizations. added the ability to open sumatra pdf

---
+++ caster_user_content/rules/apps/file_dialog.py
@@ -0,0 +1,45 @@
+from dragonfly import Repeat, Dictation, MappingRule, ShortIntegerRef
+
+from castervoice.lib.actions import Key
+
+from castervoice.lib.actions import Text
+from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
+from castervoice.lib.merge.state.short import R
+
+
+class FileDialogRule(MappingRule):
+    pronunciation = "custom file"
+    mapping = {
+        "address bar":
+            R(Key("a-d")),
+        "folder new":
+            R(Key("cs-n")),
+        "file new":
+            R(Key("a-f, w, t")),
+        "(show | file | folder) properties":
+            R(Key("a-enter")),
+        "dirrup":
+            R(Key("a-up")),
+        "go back":
+            R(Key("a-left")),
+        "go forward":
+            R(Key("a-right")),
+        "search [<text>]":
+            R(Key("a-d, tab:1") + Text("%(text)s")),
+        "(navigation | nav | left) pane":
+            R(Key("a-d, tab:2")),
+        "(center pane | (file | folder) (pane | list))":
+            R(Key("a-d, tab:3")),
+        "organize": R(Key("c-l, tab:2")),
+        "sort [headings]": R(Key("c-l, tab:5")),
+        "[file] name": R(Key("a-n")),
+        "file type": R(Key("c-l, tab:7")),
+
+    }
+    extras = [ShortIntegerRef("n", 1, 10), Dictation("text")]
+    defaults = {
+        "n": 1,
+    }
+
+def get_rule():
+    return FileDialogRule, RuleDetails(name="custom file dialog", title=["open", "save", "select"])
