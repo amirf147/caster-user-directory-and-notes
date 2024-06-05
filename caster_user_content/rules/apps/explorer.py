4eeb2f4b164909a383bd2095f861aec28b10bf0c
Amir Farhadi <amirf147@gmail.com>
2024-06-06 00:53:28 +0300

    create explorer rule
    Instead of using the ierule and transforming all of the or at least most of the phrases, i think it makes more sense to create my own rule by just copying it and making modifications to it

---
+++ caster_user_content/rules/apps/explorer.py
@@ -0,0 +1,49 @@
+from dragonfly import Dictation, MappingRule, ShortIntegerRef
+
+from castervoice.lib.actions import Key, Text
+
+from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
+from castervoice.lib.merge.state.short import R
+
+
+class ExplorerRule(MappingRule):
+    """I just copied over the IERule because i feel like I'll change a lot of 
+        the default phrases in the future.
+    """
+    pronunciation = "explore"
+    mapping = {
+        "address bar":
+            R(Key("a-d")),
+        "folder new":
+            R(Key("cs-n")),
+        "file new":
+            R(Key("a-f, w, t")),
+        "(show | file | folder) properties":
+            R(Key("a-enter")),
+        "dirrup":
+            R(Key("a-up")),
+        "go back":
+            R(Key("a-left")),
+        "go forward":
+            R(Key("a-right")),
+        "search [<text>]":
+            R(Key("a-d, tab:1") + Text("%(text)s")),
+        "(navigation | nav | left) pane":
+            R(Key("a-d, tab:2")),
+        "(center pane | (file | folder) (pane | list))":
+            R(Key("a-d, tab:3")),
+            # for the sort command below,
+            # once you've selected the relevant heading for sorting using the arrow keys, press enter
+        "sort [headings]":
+            R(Key("a-d, tab:4")),
+
+    }
+    extras = [
+        Dictation("text"),
+        ShortIntegerRef("n", 1, 1000),
+    ]
+    defaults = {"n": 1}
+
+
+def get_rule():
+    return ExplorerRule, RuleDetails(name="explore", executable="explorer")
