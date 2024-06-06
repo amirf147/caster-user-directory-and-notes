2581132c675063984e93afdfbd63a8152177bdc5
Amir Farhadi <amirf147@gmail.com>
2024-06-06 11:38:27 +0300

    create vlcrule, disabled some unused rules, removed app from bringme
    vlcrule contains mappings for speeding up and slowing down playback
    
    i'm not sure what these hmcrules are but they keep accidentally getting enabled, i might remove them or make them false
    
    so there are some error when trying to open up obsstudio so just removed from the bringme file

---
+++ caster_user_content/rules/apps/vlc.py
@@ -0,0 +1,25 @@
+from dragonfly import Repeat, Dictation, MappingRule, ShortIntegerRef
+
+from castervoice.lib.actions import Key
+
+from castervoice.lib.actions import Text
+from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
+from castervoice.lib.merge.state.short import R
+
+
+class VLCRule(MappingRule):
+    pronunciation = "media player"
+    mapping = {
+        "speed up [<n>]": R(Key("rbracket:%(n)d")),
+        "slow down [<n>]": R(Key("lbracket:%(n)d")),
+
+    }
+    extras = [
+        ShortIntegerRef("n", 1, 1001)
+    ]
+    defaults = {
+        "n": 1,
+    }
+
+def get_rule():
+    return VLCRule, RuleDetails(name="media player", executable="vlc")
