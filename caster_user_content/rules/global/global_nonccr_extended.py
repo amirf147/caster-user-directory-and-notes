b8b23f4812b74fb0eb829743518e84dbe3a980ca
Amir Farhadi <amirf147@gmail.com>
2024-06-10 23:54:43 +0300

    create global nonccr rule and add mapping
    added the window switching rule to this
    rule, added mapping for pulling up the volume on screen display from the taskbar

---
+++ caster_user_content/rules/global/global_nonccr_extended.py
@@ -0,0 +1,25 @@
+from dragonfly import MappingRule, IntegerRef, Sleep
+from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
+from castervoice.lib.actions import Key, Text
+from castervoice.lib.merge.state.short import R
+
+class GlobalNonCCRExtendedRule(MappingRule):
+    pronunciation = "global extended"
+    mapping = {
+        "switch <n>":
+            R(Key("w-%(n)d/3")),
+        "switch minus [<n>]":
+            R(Key("w-t/3, up:%(n)d, enter")),
+        "volume output":
+            R(Key("w-b/3, up:3, enter/9")),
+    }
+    extras = [
+        IntegerRef("n", 1, 10),
+    ]
+    defaults = {
+        "n": 1
+    }
+
+def get_rule():
+    details = RuleDetails(name="Global Non CCR Extended")
+    return GlobalNonCCRExtendedRule, details
