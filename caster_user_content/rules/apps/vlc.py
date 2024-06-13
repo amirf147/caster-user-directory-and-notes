bf7b638c5ea28d847154c5654ca2b0323b7daf11
Amir Farhadi <amirf147@gmail.com>
2024-06-13 21:00:47 +0300

    update vlc rule with file opening mapping
    created a mapping for the keyboard shortcut for opening a file in        vlc

--- caster_user_content/rules/apps/vlc.py
+++ caster_user_content/rules/apps/vlc.py
@@ -12,6 +12,7 @@ class VLCRule(MappingRule):
     mapping = {
         "speed up [<n>]": R(Key("rbracket:%(n)d")),
         "slow down [<n>]": R(Key("lbracket:%(n)d")),
+        "file open" : R(Key("c-o")),
 
     }
     extras = [
