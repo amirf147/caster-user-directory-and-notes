168a11312995f116ac157fa2ba66634be05780dc
Amir Farhadi <amirf147@gmail.com>
2024-06-15 13:04:54 +0300

    Update global_nonccr_extended.py
    added mappings for window manipulation including movement and resizing

--- caster_user_content/rules/global/global_nonccr_extended.py
+++ caster_user_content/rules/global/global_nonccr_extended.py
@@ -6,10 +6,24 @@ from castervoice.lib.merge.state.short import R
 class GlobalNonCCRExtendedRule(MappingRule):
     pronunciation = "global extended"
     mapping = {
+        # Window Switching
         "switch <n>":
             R(Key("w-%(n)d/3")),
         "switch minus [<n>]":
             R(Key("w-t/3, up:%(n)d, enter")),
+        
+        # Window Manipulation
+        "window move":
+            R(Key("a-space/5, m")),
+        "window resize right":
+            R(Key("a-space/5, s/3, right")),
+        "window resize left":
+            R(Key("a-space/5, s/3, left")),
+        "window resize up":
+            R(Key("a-space/5, s/3, up")),
+        "window resize down":
+            R(Key("a-space/5, s/3, down")),
+
         "volume output":
             R(Key("w-b/3, up:3, enter/9")),
     }
