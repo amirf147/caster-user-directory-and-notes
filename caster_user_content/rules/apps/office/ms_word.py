ae8f1c261d9b3fd0ece9f468f0cec4d24f94089b
Amir Farhadi <amirf147@gmail.com>
2024-07-12 01:43:43 +0300

    Create ms_word.py
    create custom rule for microsoft word by copying in and adding to the built in one

---
+++ caster_user_content/rules/apps/office/ms_word.py
@@ -0,0 +1,23 @@
+from dragonfly import Dictation, MappingRule, ShortIntegerRef
+from castervoice.lib.actions import Key
+from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
+from castervoice.lib.merge.state.short import R
+
+
+class CustomMSWordRule(MappingRule):
+    mapping = {
+        "insert image": R(Key("alt, n, p")),
+        "file open": R(Key("c-o")),
+        "file retain": R(Key("c-s")),
+        "file new": R(Key("c-n")),
+    }
+    extras = [
+        Dictation("dict"),
+        ShortIntegerRef("n", 1, 100),
+    ]
+    defaults = {"n": 1, "dict": "nothing"}
+
+
+def get_rule():
+    details = RuleDetails(name="Custom Microsoft Word", executable="winword")
+    return CustomMSWordRule, details
