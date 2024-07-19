4e5812c9d125aaea65085e759a5883e58bd51ff2
Amir Farhadi <amirf147@gmail.com>
2024-07-19 08:15:42 +0300

    update gemini and words
    added the ability to focus the responses in the gemini web app
    
    change the word bat back to brov for pressing the letter b

--- caster_user_content/rules/websites/gemini.py
+++ caster_user_content/rules/websites/gemini.py
@@ -1,4 +1,4 @@
-from dragonfly import MappingRule, ShortIntegerRef, Repeat
+from dragonfly import MappingRule, ShortIntegerRef, Repeat, Pause
 from castervoice.lib.actions import Key
 from castervoice.lib.merge.state.short import R
 from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
@@ -8,6 +8,15 @@ class GeminiRule(MappingRule):
     mapping = {
         "compose [box]":
             R(Key("f1")),
+        
+        # bring focus to the response area above the compose box so you can scroll the response
+        # using keyboard keys
+        # ever since an update to gemini, no longer able to "s-tab" out of the compose box
+        # so using f6 and multiple presses of the tab key is used as a workaround
+        "response":
+            R(Key("f6/3, f6, tab") +
+              Pause("40") + Key("tab:24") +
+              Pause("60") + Key("end, up:10"))
     }
 
 def get_rule():
