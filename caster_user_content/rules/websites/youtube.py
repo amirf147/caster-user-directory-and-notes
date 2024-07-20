fdeeca494236809619835e4518ab870ae50f4277
Amir Farhadi <amirf147@gmail.com>
2024-07-20 10:23:55 +0300

    Update youtube.py
    tried to fix the you tube rule but failed and will have to return to it later

--- caster_user_content/rules/websites/youtube.py
+++ caster_user_content/rules/websites/youtube.py
@@ -1,15 +1,20 @@
-from dragonfly import MappingRule, ShortIntegerRef, Repeat
-from castervoice.lib.actions import Key
+from dragonfly import MappingRule, ShortIntegerRef, Repeat, Pause
 from castervoice.lib.merge.state.short import R
 from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
+from castervoice.lib.actions import Key
 
 class YoutubeRule(MappingRule):
     pronunciation = "youtube rule"
     mapping = {
+        # TODO: Not working, fix this when I have time
         "speed up [<n>]":
-            R(Key("colon"))*Repeat(extra="n"),
+            R(Key("escape/8, escape") +
+              Pause("100") + Key("i/3") +
+              Key("colon:%(n)d")),
         "slow down [<n>]":
-            R(Key("semicolon"))*Repeat(extra="n"),
+            R(Key("escape/8, escape") +
+              Pause("100") + Key("i/3") +
+              Key("semicolon:%(n)d")),
     }
     extras = [
         ShortIntegerRef("n", 1, 9),
