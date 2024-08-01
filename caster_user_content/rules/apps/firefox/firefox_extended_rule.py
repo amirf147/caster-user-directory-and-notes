b7f4ae39d2ee6307901bf4d20f962d91cef56f71
Amir Farhadi <amirf147@gmail.com>
2024-08-01 10:28:59 +0300

    Update firefox_extended_rule.py
    added chatgpt to websites

--- caster_user_content/rules/apps/firefox/firefox_extended_rule.py
+++ caster_user_content/rules/apps/firefox/firefox_extended_rule.py
@@ -65,6 +65,7 @@ class FirefoxExtendedRule(MappingRule):
             "subscriptions": "https://www.youtube.com/feed/subscriptions",
             "school": "tuas.fi",
             "gemini": "https://gemini.google.com/app",
+            "chat gee pea tea": "chatgpt.com",
         }),
         Dictation("query"),
     ]
