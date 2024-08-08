3d55d78640d3aae57697f8bbe2cbd270a6c136d9
Amir Farhadi <amirf147@gmail.com>
2024-08-08 17:38:52 +0300

    update ms_word.py
    added ability to apply normal and heading styles

--- caster_user_content/rules/apps/office/ms_word.py
+++ caster_user_content/rules/apps/office/ms_word.py
@@ -1,4 +1,4 @@
-from dragonfly import Dictation, MappingRule, ShortIntegerRef, Pause
+from dragonfly import Dictation, MappingRule, ShortIntegerRef, Pause, IntegerRef
 from castervoice.lib.actions import Key
 from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
 from castervoice.lib.merge.state.short import R
@@ -9,14 +9,23 @@ class CustomMSWordRule(MappingRule):
         "insert image": R(Key("alt, n, p")),
         "file open": R(Key("c-o")),
         "file retain": R(Key("c-s")),
+        "file new": R(Key("c-n")),
+
+        # Initial attempt at getting to file save as
         "file custom retain": 
             R(Key("a-f, a") +
               Pause("60") + Key("c")),
-        "file new": R(Key("c-n")),
+
+        # Applying styles
+        "apply normal [style]":
+            R(Key("cs-n")),
+        "apply heading <n3>":
+            R(Key("ac-%(n3)d")),
     }
     extras = [
         Dictation("dict"),
         ShortIntegerRef("n", 1, 100),
+        IntegerRef("n3", 1, 4)
     ]
     defaults = {"n": 1, "dict": "nothing"}
 
