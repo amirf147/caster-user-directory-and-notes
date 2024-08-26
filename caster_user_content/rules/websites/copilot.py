c9ca098427c81b65ebcd08bec2286f595ade2f7a
Amir Farhadi <amirf147@gmail.com>
2024-08-26 10:57:42 +0300

    create and enable copilot rule
    made it a copy of the other llm chat websites with just the ability to focus the compose box

---
+++ caster_user_content/rules/websites/copilot.py
@@ -0,0 +1,13 @@
+from dragonfly import MappingRule, ShortIntegerRef, Repeat
+from castervoice.lib.actions import Key
+from castervoice.lib.merge.state.short import R
+from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
+
+class CopilotRule(MappingRule):
+    mapping = {
+        "compose [box]":
+            R(Key("f1")),
+    }
+
+def get_rule():
+    return CopilotRule, RuleDetails(name="copilot", title="copilot")
