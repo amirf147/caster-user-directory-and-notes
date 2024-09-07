655b0c682a89948f1138dc0831a48de58656afbc
Amir Farhadi <amirf147@gmail.com>
2024-09-07 15:05:34 +0300

    Update file_dialog.py
    enabled the file dialog rule to work with file uploading dialogs

--- caster_user_content/rules/apps/explorer/file_dialog.py
+++ caster_user_content/rules/apps/explorer/file_dialog.py
@@ -42,4 +42,4 @@ class FileDialogRule(MappingRule):
     }
 
 def get_rule():
-    return FileDialogRule, RuleDetails(name="custom file dialog", title=["open", "save", "select"])
+    return FileDialogRule, RuleDetails(name="custom file dialog", title=["open", "save", "select", "file upload"])
