d94cddeb54502de29fcfd79880bf7850f05f6958
Amir Farhadi <amirf147@gmail.com>
2024-11-11 15:35:03 +0200

    Create file_explorer.py
    Created a mapping rule for file explorer intended to contain a lot of the commands that were originally in the merge rule.

---
+++ caster_user_content/rules/apps/explorer/file_explorer.py
@@ -0,0 +1,59 @@
+from dragonfly import Repeat, Dictation, MappingRule, ShortIntegerRef, Choice
+
+from castervoice.lib.actions import Key
+
+from castervoice.lib.actions import Text
+from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
+from castervoice.lib.merge.state.short import R
+
+
+class FileExplorerRule(MappingRule):
+    mapping = {
+        "address bar":
+            R(Key("a-d")),
+        "folder new":
+            R(Key("cs-n")),
+        "file new":
+            R(Key("a-f, w, t")),
+        "(show | file | folder) properties":
+            R(Key("a-enter")),
+        "dirrup [<n>]":
+            R(Key("a-up:%(n)d")),
+        "go back [<n>]":
+            R(Key("a-left:%(n)d")),
+        "go forward [<n>]":
+            R(Key("a-right:%(n)d")),
+        "search [<text>]":
+            R(Key("a-d, tab:1") + Text("%(text)s")),
+        "(navigation | nav | left) pane":
+            R(Key("a-d, tab:2")),
+        "(center pane | (file | folder) (pane | list))":
+            R(Key("a-d, tab:3")),
+        "organize": R(Key("c-l, tab:2")),
+        "sort [headings]": R(Key("c-l, tab:5")),
+        "[file] name": R(Key("a-n")),
+        "file type": R(Key("c-l, tab:7")),
+
+        # Navigating via address bar
+        "go <path>":
+            R(Key("a-d/5") + Text("%(path)s") + Key("enter")),
+        
+        "fit column":
+            R(Key("a-v, s, f")),
+
+    }
+    extras = [
+        ShortIntegerRef("n", 1, 10),
+        Dictation("text"),
+        Choice("path", {
+            "[my] documents" : "C:\\Users\\amirf\\Documents\\",
+            "downloads" : "C:\\Users\\amirf\\Downloads\\",
+        }),
+        ]
+    defaults = {
+        "n": 1,
+    }
+
+def get_rule():
+    return FileExplorerRule, RuleDetails(name="File Explorer Rule",
+                                         executable="explorer")
