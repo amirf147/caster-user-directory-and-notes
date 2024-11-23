df92c00350d601485c6094221329cf2184c4718a
Amir Farhadi <amirf147@gmail.com>
2024-11-22 19:49:57 -0600

    Update gnumeric.py
    Added a command for popping up the "Insert cells" dialog.

--- caster_user_content/rules/apps/gnumeric.py
+++ caster_user_content/rules/apps/gnumeric.py
@@ -14,6 +14,7 @@ class GnumericRule(MappingRule):
         "fit width": R(Key("a-o, right, w")),
         "fit height": R(Key("a-o, right, h")),
         "match above": R(Key("c-d")),
+        "cell insert": R(Key("cs-+")),
 
     }
     extras = [
