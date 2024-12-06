3ea607827e0f6d7c97a9c1db55812ecbc19c1a95
Amir Farhadi <amirf147@gmail.com>
2024-12-06 11:19:53 -0600

    Create notepadplusplus.py
    Create the rule with zooming commands.

---
+++ caster_user_content/rules/apps/notepadplusplus.py
@@ -0,0 +1,25 @@
+from dragonfly import Repeat, Dictation, MappingRule, ShortIntegerRef
+
+from castervoice.lib.actions import Key, Text, Mouse
+
+from castervoice.lib.actions import Text
+from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
+from castervoice.lib.merge.state.short import R
+
+
+class CustomNPPRule(MappingRule):
+
+    mapping = {
+        "zoom in <n2>": R(Key("c-npadd:%(n2)d")),
+        "zoom out <n2>": R(Key("c-npsub:%(n2)d")),
+    }
+    extras = [
+        Dictation("text"),
+        ShortIntegerRef("n", 1, 1000),
+        ShortIntegerRef("n2", 1, 10),
+    ]
+    defaults = {"n": 1}
+
+
+def get_rule():
+    return CustomNPPRule, RuleDetails(name="Custom notepad plus plus", executable="notepad++")
