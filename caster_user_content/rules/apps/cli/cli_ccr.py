6a14fcd2005e0b0b895b851a185444379cbf3d65
Amir Farhadi <amirf147@gmail.com>
2024-12-23 05:42:23 -0600

    Moved windows terminal commands into cli_ccr
    Rather than having a dedicated rule for each type of terminal, I'll start with just having a generic cli rule for now.

--- caster_user_content/rules/apps/cli/cli_ccr.py
+++ caster_user_content/rules/apps/cli/cli_ccr.py
@@ -1,4 +1,4 @@
-from dragonfly import ShortIntegerRef, Text
+from dragonfly import ShortIntegerRef, Text, Choice
 
 from castervoice.lib.actions import Key
 
@@ -13,7 +13,31 @@ class CommandLineCCRRule(MergeRule):
 
     mapping = {
         "axe": R(Key("a-backspace")),
+        
+        "dirrup":
+            R(Text("cd ../ ; ls;") + Key("enter")),
+        
+        "go <path>": R(Text("cd %(path)s") + Key("enter")),
+
+        "go": R(Key("c, d, space")),
+
+        # Executables
+        "<exe>": R(Text("%(exe)s")),
+
+        # Python exe
+        "pi debug": R(Text("$p312 -m pdb ")),
     }
+    extras = [
+        Choice("path", {
+            "pi folder": "C:/Users/amirf/python",
+            "caster user": "C:/Users/amirf/AppData/Local/caster",
+            "caster rules": "C:/Users/amirf/AppData/Local/caster/caster_user_content/rules",
+            "caster apps": "C:/Users/amirf/AppData/Local/caster/caster_user_content/rules/apps",
+        }),
+        Choice("exe", {
+            "pi twelve": "$p312", # Environment Variable
+        }),
+    ]
 
 _executables = [
     "WindowsTerminal"
