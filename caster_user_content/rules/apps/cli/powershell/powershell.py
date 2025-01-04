c69fa416cbbb061b9ef366cc7fdaf955fabdaa8f
Amir Farhadi <amirf147@gmail.com>
2025-01-03 19:32:34 -0600

    Create powershell.py
    Copied over cli rule with relevant commands. Made the Choice extra use a environment variable for its dictionary. I will use this convention for my Choice dictionaries in other rules going forward and I will update other rules in future commits.

---
+++ caster_user_content/rules/apps/cli/powershell/powershell.py
@@ -0,0 +1,38 @@
+from dragonfly import Function, Repeat, Choice, Dictation, MappingRule, Pause, ShortIntegerRef, Mimic
+
+from castervoice.lib.actions import Key, Mouse
+
+from castervoice.lib import navigation
+from castervoice.lib.actions import Text
+from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
+from castervoice.lib.merge.state.short import R
+
+from caster_user_content import environment_variables as ev
+
+class PowershellRule(MappingRule):
+    mapping = {
+        "go <path>": R(Text("cd %(path)s") + Key("enter")),
+
+        "dirrup": R(Text("cd ../ ; ls;") + Key("enter")),
+        "dir home": R(Text("cd; ls;") + Key("enter")),
+
+        # "pi twelve": R(Text("$p312 ")),
+        "pi quit": R(Text("quit()") + Key("enter")),
+        "pi exit": R(Text("exit()") + Key("enter")),
+
+        # sqlite
+        "see exit": R(Text(".exit") + Key("enter")),
+
+        "wiper": R(Text("clear") + Key("enter")),
+
+        # Redmine
+        "start redmine": R(Mimic("go redmine") + Pause("50") + Text("bundle exec rails server -e production") + Key("enter")),
+    }
+    extras = [
+        Choice("path", ev.PATHS),
+    ]
+    defaults = {
+    }
+
+def get_rule():
+    return PowershellRule, RuleDetails(name="Powershell", executable="powershell")
