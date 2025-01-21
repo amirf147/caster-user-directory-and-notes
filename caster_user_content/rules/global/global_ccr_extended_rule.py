b40a5f59ec14bc928805dbddaa8d47d3975c78a6
Amir Farhadi <amirf147@gmail.com>
2025-01-21 09:03:49 -0600

    Comment out "rash"/"lash" commands to prevent accidental triggers
    Disabled these voice commands as they were too easily misrecognized during normal speech.

--- caster_user_content/rules/global/global_ccr_extended_rule.py
+++ caster_user_content/rules/global/global_ccr_extended_rule.py
@@ -18,8 +18,8 @@ class GlobalCCRExtendedRule(MergeRule):
         "go back [<n>]": R(Key("a-left:%(n)d")),
         "scratch [<n101>]": R(Key("c-backspace:%(n101)d")),
         "dear [<n101>]": R(Key("c-del:%(n101)d")),
-        "rash": R(Key("s-end")),
-        "lash": R(Key("s-home")),
+        # "rash": R(Key("s-end")),
+        # "lash": R(Key("s-home")),
         "win key <query>": R(Key("win") + Pause("30") + Text("%(query)s")),
         
         # adding an empty line above or below the cursor
