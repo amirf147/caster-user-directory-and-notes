96ee781e3e46b4e341852224ae5ba8f5adde45a0
Amir Farhadi <amirf147@gmail.com>
2025-05-02 12:20:13 -0500

    Remove unused Python 8 command from PowerShell rule
    This commit removes the unused Python 8 command from the PowerShell rule, keeping only the active Python 12 and Python 10 commands.

--- caster_user_content/rules/apps/cli/powershell/powershell.py
+++ caster_user_content/rules/apps/cli/powershell/powershell.py
@@ -14,7 +14,7 @@ from caster_user_content.util.text import text_to_clipboard
 
 PYTHON_12 = ev.EXECUTABLES["pi twelve"]
 PYTHON_10 = ev.EXECUTABLES["pi ten"]
-PYTHON_8 = ev.EXECUTABLES["pi eight"]
+# PYTHON_8 = ev.EXECUTABLES["pi eight"]
 
 class PowershellRule(MappingRule):
     mapping = {
@@ -107,7 +107,7 @@ class PowershellRule(MappingRule):
         # Python
         "pi twelve <python_command>": R(Text(PYTHON_12) + Key("space") + Text("%(python_command)s")),
         "pi ten <python_command>": R(Text(PYTHON_10) + Key("space") + Text("%(python_command)s")),
-        "pi eight <python_command>": R(Text(PYTHON_8) + Key("space") + Text("%(python_command)s")),
+        # "pi eight <python_command>": R(Text(PYTHON_8) + Key("space") + Text("%(python_command)s")),
         "pip <pip_command>": R(Text("pip %(pip_command)s")),
 
         # Variables
