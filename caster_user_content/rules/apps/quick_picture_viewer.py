1e4a97a8f26db26cdee59888114553cb0b88dba3
Amir Farhadi <amirf147@gmail.com>
2025-05-14 04:13:28 -0500

    Add new voice commands to QuickPictureViewerRule
    This update introduces several new voice commands to the QuickPictureViewerRule in Caster:
    
    - Added "zoom auto" command for automatic zoom adjustment.
    - Added "slideshow" command to start a slideshow.
    - Added "show info" command to display file information.
    
    These additions enhance the user's ability to interact with the Quick Picture Viewer through voice commands.

--- caster_user_content/rules/apps/quick_picture_viewer.py
+++ caster_user_content/rules/apps/quick_picture_viewer.py
@@ -11,8 +11,14 @@ class QuickPictureViewerRule(MappingRule):
         "file open": R(Key("c-o")),
         "file retain": R(Key("c-s")),
         "file custom retain": R(Key("cs-s")),
+
         "zoom in [<n2>]": R(Key("c-equals:%(n2)d")),
         "zoom out [<n2>]": R(Key("c-minus:%(n2)d")),
+        "zoom auto": R(Key("cs-a")),
+
+        "slideshow": R(Key("s-f5")),
+
+        "show info": R(Key("c-i")),
     }
     extras = [
         ShortIntegerRef("n2", 1, 10),
