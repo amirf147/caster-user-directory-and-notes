74d270a25a61c6802d0810236df3a43bb7cb6688
Amir Farhadi <amirf147@gmail.com>
2025-06-30 10:32:40 -0500

    Add Trello navigation and card management commands
    Added voice commands for Trello navigation and card management:
    - Added sidebar and board menu navigation
    - Added due date and checklist commands
    - Added card movement commands: card over/under [n] and move over/under [n]
    - Added support for numbered repeats

--- caster_user_content/rules/websites/trello.py
+++ caster_user_content/rules/websites/trello.py
@@ -1,4 +1,4 @@
-from dragonfly import MappingRule, Choice
+from dragonfly import MappingRule, Choice, IntegerRef, Repeat
 from castervoice.lib.actions import Key
 from castervoice.lib.merge.state.short import R
 from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
@@ -6,8 +6,16 @@ from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
 class TrelloRule(MappingRule):
 
     mapping = {
+        "sidebar": R(Key("[")),
+        "board menu": R(Key("]")),
         "card new": R(Key("n")),
         "card archive": R(Key("c")),
+        "due date": R(Key("d")),
+        "checklist": R(Key("minus")),
+        "card over [<n>]": R(Key("</20")) * Repeat(extra='n'),
+        "card under [<n>]": R(Key(">/20")) * Repeat(extra='n'),
+        "move over [<n>]": R(Key("comma/20")) * Repeat(extra='n'),
+        "move under [<n>]": R(Key("./20")) * Repeat(extra='n'),
         "label <color>": R(Key("l/30, %(color)s, tab, enter, escape")),
     }
 
@@ -20,9 +28,11 @@ class TrelloRule(MappingRule):
             "purple": "5",
             "blue": "6",
         }),
+        IntegerRef("n", 1, 10),
     ]
 
     defaults = {
+        "n": 1,
     }
 
 def get_rule():
