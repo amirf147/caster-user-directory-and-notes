2fbbd50058ff4b774dab1a93d25ddd7e3b86ac8f
Amir Farhadi <amirf147@gmail.com>
2025-08-27 05:42:16 +0300

    Refactor VSCode cursor rule imports and cleanup
    - Removed unused imports: Function, Repeat, Choice, Pause, navigation
    - Removed Mouse import from castervoice.lib.actions
    - Removed unused Text import
    - Cleaned up import organization
    - No functional changes, only import optimizations

--- caster_user_content/rules/apps/vscode/cursor.py
+++ caster_user_content/rules/apps/vscode/cursor.py
@@ -1,8 +1,7 @@
-from dragonfly import Function, Repeat, Choice, Dictation, MappingRule, Pause, ShortIntegerRef
+from dragonfly import Dictation, MappingRule, ShortIntegerRef
 
-from castervoice.lib.actions import Key, Mouse
+from castervoice.lib.actions import Key
 
-from castervoice.lib import navigation
 from castervoice.lib.actions import Text
 from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
 from castervoice.lib.merge.state.short import R
