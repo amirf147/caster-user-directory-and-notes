343b29ace5a2a7fc6d652f8da6598fff6209ad82
Amir Farhadi <amirf147@gmail.com>
2025-10-28 18:06:24 +0200

    Fix pip commands for python in command line
    This commit modifies the pip and pip install voice commands in the Python rule to invoke pip as a module.
    
    The pip command was changed from pip to -m pip.
    The pip install command was changed from pip install to -m pip install.
    
    This makes the commands actually work whereas previously there were not working.

--- caster_user_content/rules/apps/cli/cli_support.py
+++ caster_user_content/rules/apps/cli/cli_support.py
@@ -64,8 +64,8 @@ LIST_COMMANDS = {
 
 PYTHON_COMMANDS = {
     "create virtual": "-m venv .venv",
-    "pip": "pip ",
-    "pip install": "pip install ",
+    "pip": "-m pip ",
+    "pip install": "-m pip install ",
 }
 
 PIP_COMMANDS = {
