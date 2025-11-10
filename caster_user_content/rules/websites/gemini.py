61c44fc9c1ca211288607869f7e6f45c16445111
Amir Farhadi <amirf147@gmail.com>
2025-11-10 13:26:54 +0200

    Update Gemini rule with mode switching commands
    - Removed non-functional "focus compose [box]" and "response" commands
    - Added "pro mode" command to switch to pro mode
    - Added "fast mode" command to switch to fast mode
    
    These changes improve compatibility with the latest Gemini interface by removing broken commands and adding new ones for mode switching.

--- caster_user_content/rules/websites/gemini.py
+++ caster_user_content/rules/websites/gemini.py
@@ -6,18 +6,22 @@ from castervoice.lib.ctrl.mgr.rule_details import RuleDetails
 class GeminiRule(MappingRule):
     pronunciation = "gemini rule"
     mapping = {
-        "focus compose [box]":
-            R(Key("f1")),
+        # "focus compose [box]": # No longer is being recognized in Waterfox, instead it just toggles the side bar or something
+        #     R(Key("f1")),
         
         # bring focus to the response area above the compose box so you can scroll the response
         # using keyboard keys
         # ever since an update to gemini, no longer able to "s-tab" out of the compose box
         # so using f6 and multiple presses of the tab key is used as a workaround
-        "response":
-            R(Key("f6/3, f6") +
-              Pause("40") + Key("tab") +
-              Pause("40") + Key("tab:24") +
-              Pause("60") + Key("end, up:10"))
+        # "response":
+        #     R(Key("f6/3, f6") +
+        #       Pause("40") + Key("tab") +
+        #       Pause("40") + Key("tab:24") +
+        #       Pause("60") + Key("end, up:10"))
+        "pro mode": # Requires that you are in the message composer box, switches to pro mode from fast mode 
+            R(Key("tab:3, space, tab, enter")),
+        "fast mode": # Requires that you are in the message composer box, switches to fast mode from pro mode 
+            R(Key("tab:3, space, enter")),
     }
 
 def get_rule():
